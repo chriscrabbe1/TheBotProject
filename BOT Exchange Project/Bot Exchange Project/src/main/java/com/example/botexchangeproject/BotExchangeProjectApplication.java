64393ea1e8338cc35ce2aef8689164c4a4e047e4
chriscrabbe1 <144935047+chriscrabbe1@users.noreply.github.com>
2023-11-06 18:54:08 +0000

    re-wrote code and "returnToken"

--- BOT Exchange Project/Bot Exchange Project/src/main/java/com/example/botexchangeproject/BotExchangeProjectApplication.java
+++ BOT Exchange Project/Bot Exchange Project/src/main/java/com/example/botexchangeproject/BotExchangeProjectApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+
 @SpringBootApplication
 public class BotExchangeProjectApplication {
 
-    public static void main(String[] args) { SpringApplication.run(BotExchangeProjectApplication.class, args);
+    public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException
+    { SpringApplication.run(BotExchangeProjectApplication.class, args);
+
+        Login.returnToken();
 
     }
+
+
 
 }
 
